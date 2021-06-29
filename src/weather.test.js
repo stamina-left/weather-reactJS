29dcd5911746d4f0aab14d43eb87964a1231134f
Albert <stamina.left@gmail.com>
2021-06-29 10:09:21 +0700

    To test if user is able to check weather.

---
+++ src/weather.test.js
@@ -0,0 +1,7 @@
+import { fireEvent, screen, render, queryByTestId } from '@testing-library/react';
+import React from 'react';
+import Weather from './weather';
+
+test("Enter the name of a city shows weather condition.", () => {
+
+});
