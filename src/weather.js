9cb3078edf461160995e1e4b5264ad7563673d87
Albert <stamina.left@gmail.com>
2021-06-29 10:39:16 +0700

    For now, it only returns a Temp & Weather.

---
+++ src/weather.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+export default function Weather() {
+    const [temperature, setTemperature] = useState(null);
+    const [weather, setWeather] = useState(null);
+
+}
